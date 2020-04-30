4abfe0f84071720bb53c3466aff6ac3ed7e461f1
Ajnura Gasanova <ajnura.gasanova.1999@mail.ru>
2020-04-30 19:52:06 +0700

    Реализация с помощью наследования

---
+++ KlassStrok/Project1/Unit.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include<vector>
+
+using namespace std;
+
+/*
+* –реализаци¤ с помощью наследовани¤.
+*/
+
+class TMyString {
+public:
+	TMyString() {};
+	virtual ~TMyString() {};
+	virtual int getSize() = 0; // получить размерность массива
+	virtual void setSize(int newSize) = 0; // установить размерность массива
+	virtual void setChar(int pos, char ch) = 0; // установить символ на текущей позиции
+	virtual char operator[](int i) = 0;
+};
+
+class TMyDoubleString : public TMyString {
+public:
+	TMyDoubleString(int newSize);
+	TMyDoubleString(double* str, int len);
+	virtual ~TMyDoubleString();
+
+	int getSize();
+	void setSize(int newSize);
+	void setChar(int pos, char ch);
+	char operator[](int i);
+	friend TMyDoubleString operator+(TMyDoubleString& str1, TMyDoubleString& str2);
+protected:
+	vector<double> elems;
+};
+
+class TMyCharString : public TMyString {
+public:
+	TMyCharString();
+	TMyCharString(int newSize);
+	TMyCharString(char* str);
+	TMyCharString(char* str, int newSize);
+	virtual ~TMyCharString();
+
+	int getSize();
+	void setSize(int newSize);
+	void setChar(int pos, char ch);
+	char operator[](int i);
+	friend TMyCharString operator+(TMyCharString& str1, TMyCharString& str2);
+protected:
+	vector<char> elems;
+};
+
+
+// =================================================================================================
+
+
+
+
+class Unit1
+{
+
+};
